de4ccfccbacd992dfbe5d07f26f88167d0f2e335
Aakash S P <92024953+aakashsp30@users.noreply.github.com>
2026-05-22 14:17:20 +0000

    Loading state

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ async function getWeatherData(location, unit) {
   const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=${unitGroup}&key=${API_KEY}&contentType=json`;
 
   try {
+    showLoading();
+
     const response = await fetch(url);
 
     if (!response.ok) {
@@ -16,6 +18,8 @@ async function getWeatherData(location, unit) {
     return processed;
   } catch (error) {
     console.error("Error fetching weather data:", error);
+  } finally {
+    hideLoading();
   }
 }
 
@@ -97,4 +101,15 @@ function setBackground(icon) {
   } else {
     body.className = "";
   }
+}
+
+const loading = document.getElementById("loading");
+const weatherDisplay = document.getElementById("weather-display");
+function showLoading() {
+  loading.style.display = "block";
+  weatherDisplay.innerHTML = "";
+}
+
+function hideLoading() {
+  loading.style.display = "none";
 }
